0b3f0c538b671c85623251b437dd8f39a2e96067
zxzs777 <153170667+zxzs777@users.noreply.github.com>
2023-12-07 19:44:36 +0800

    Add files via upload

---
+++ SingalRClient/SingalRClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:44341/dataHub")
+    .Build();
+
+connection.On<string>("ReceiveData", (data) =>
+{
+    Console.WriteLine( string.Join(", ", data));
+    //Console.WriteLine("Received data from server: " + string.Join(", ", data));
+});
+
+await connection.StartAsync();
+
+Console.ReadKey();
